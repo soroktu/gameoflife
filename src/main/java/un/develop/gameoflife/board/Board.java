f888f863cbf9201a38ce164af3ea5ff52bbaa2a0
Andrey Durow <druff@mail.ru>
2018-07-17 14:08:59 +0300

    new entities (board & cell)

---
+++ src/main/java/un/develop/gameoflife/board/Board.java
@@ -0,0 +1,45 @@
+package un.develop.gameoflife.board;
+
+import un.develop.gameoflife.cell.Cell;
+
+import javax.persistence.Entity;
+import java.util.List;
+
+@Entity
+public class Board {
+
+    private int edgeLength;
+    private List<Cell> liveCells;
+
+    public Board(int edgeLength) {
+        this.edgeLength = edgeLength;
+    }
+
+    public int getEdgeLength() {
+        return edgeLength;
+    }
+
+    public void setEdgeLength(int edgeLength) {
+        this.edgeLength = edgeLength;
+    }
+
+    public List<Cell> getLiveCells() {
+        return liveCells;
+    }
+
+    public void setLiveCells(List<Cell> liveCells) {
+        this.liveCells = liveCells;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Board))
+            return false;
+        if (obj == this)
+            return true;
+        Board structure = (Board) obj;
+
+        return this.edgeLength == structure.getEdgeLength() && this.liveCells.containsAll(structure.getLiveCells());
+    }
+
+}
