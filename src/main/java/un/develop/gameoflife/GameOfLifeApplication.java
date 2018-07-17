a43f82e0d4e53db88cd2ffb2b21fc15dfcae2ec5
Andrey Durow <druff@mail.ru>
2018-07-17 13:48:04 +0300

    project after spring initializr

---
+++ src/main/java/un/develop/gameoflife/GameOfLifeApplication.java
@@ -0,0 +1,12 @@
+package un.develop.gameoflife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameOfLifeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameOfLifeApplication.class, args);
+    }
+}
